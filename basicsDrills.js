9ca9083f5972b7d8e735377a3826586dd2b01968
AwallenceCode <awallencecode@gmail.com>
2025-12-11 17:08:16 +0200

    writed task

---
+++ basicsDrills.js
@@ -0,0 +1,23 @@
+const prompt = require("prompt-sync")();
+
+let deposit, tips, numberOfPeople;
+
+deposit = Number(prompt("Enter your usdt deposit -> "));
+if (isNaN(deposit) ||  deposit <= 0 ) { console.log("Deposit must be positive number!"); process.exit(); }
+
+tips = Number(prompt("Enter your tips for barmen (0 - 100%) -> "));
+if (isNaN (tips) || tips < 0 || tips > 100) { tips = 10; }
+
+numberOfPeople = Number(prompt("Enter count your friend -> "));
+if (isNaN(numberOfPeople) || numberOfPeople <= 0) { console.log("Count of people must be a positive!"); process.exit(); }
+
+let sumTips = deposit * (tips / 100);
+
+let sumWithTips = deposit + sumTips;
+
+let sumInPerson = sumWithTips / numberOfPeople;
+
+console.log(`\nYour deposit -> ${deposit}`);
+console.log(`Your tips -> (${tips}%): ${sumTips} USDT`);
+console.log(`All sum -> ${sumWithTips} USDT`);
+console.log(`In person -> ${sumInPerson}`);
